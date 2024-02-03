c29bb217547d25f9c738877d42de0aac7c761e50
Ekaterina Kalinina <kalinia_weld@mail.ru>
2024-02-03 20:51:34 +0300

    Solution of Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,3 +2,16 @@
 // которая принимает на вход целое число из отрезка [10, 99] 
 // и показывает наибольшую цифру числа.
 
+System.Console.Writeline("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 10;
+int b = number % 10;
+
+if (a > b)
+{
+    Console.Write("a");
+}
+else
+{
+    Console.Write("b");
+}
